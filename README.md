46ceaf22ee5661a21b54a1a726bf8488c5b99853
Zain Coder <zainolamzar.work@gmail.com>
2024-11-27 01:02:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# voteguard-backend" 
---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
